2b05dd64de9e2670f365c0ef4c99f1942133f27b
Aditya Mahadevan <aditya@Amalas-MacBook-Pro.local>
2016-05-06 15:02:33 -0400

    improved readability

--- fiberOptics.py
+++ fiberOptics.py
@@ -6,9 +6,9 @@
 
 usage = "fiberOptics [--options]"
 description = "the greatest code in the world"
-author = "reed essick and Alvaro the spanish wonder"
+author = "Reed Essick and Alvaro Fernandez"
 
-#-------------------------------------
+#---------------------------------------------------------------------
 
 import time
 
@@ -21,19 +21,19 @@ matplotlib.use("Agg")
 #from matplotlib import pyplot as plt
 import pylab as plt
 
-#-------------------------------------
+#---------------------------------------------------------------------
 
 global known_columns
 global known_columns_types
 
 #SETUPS
-known_setups = ["F","FF","FT","FFF","FTF"] ### the list of possible setups F = Fiber and T = Feedthrough
+known_setups = ["F","FF","FT","FFF","FTF"] #the list of possible setups F = Fiber and T = Feedthrough
 
-kc_F       = ["day", "month", "year", "hour", "minute", "redPower(mW)", "greenPowerIn(mW)", "coupler", \
-"fiberFirstConnector", "fiber", "greenPowerOut1(mW)"] ### the list of columns that each data point MUST have
-kc_setup_F       = ["coupler", "fiberFirstConnector", "fiber"] ### the list of columns that define
+kc_F = ["day", "month", "year", "hour", "minute", "redPower(mW)", "greenPowerIn(mW)", "coupler", \
+"fiberFirstConnector", "fiber", "greenPowerOut1(mW)"] ###the list of columns that each data point MUST have
+kc_setup_F = ["coupler", "fiberFirstConnector", "fiber"] ###the list of columns that define
 kct_F = [int, int, int, int, int, float, float, str, str, str, float]
-### the types for the columns. THE ORDER MATTERS and this must have the same length as known_columns
+#the types for the columns. THE ORDER MATTERS and this must have the same length as known_columns
 
 kc_FF = ["day", "month", "year", "hour", "minute", "redPower(mW)", "greenPowerIn(mW)", "coupler", "fiber1FirstConnector", \
 "fiber1", "greenPowerOut1(mW)", "sleeve", "fiber2FirstConnector", "fiber2", "greenPowerOut2(mW)"]
@@ -93,21 +93,18 @@ powerList = [{'name': "Very High Power", 'min': 250, 'max': 350}, {'name': "High
 {'name': "Medium Power", 'min': 100, 'max': 200}, {'name': "Low Power", 'min': 50, 'max': 100}, \
 {'name': "Very Low Power", 'min': 0, 'max': 50}]
 
-#------------------------------------
+#---------------------------------------------------------------------
 
 class dataPoint(object):
-
 
     def __init__(self, name):
         self.name = name
         self.setup = None
-    #        self.data = dict( (col, None) for col in known_columns )
+        #self.data = dict((col, None) for col in known_columns)
+        self.data = dict() #equivalently, self.data = {}
 
-#        ### an equivalent block of code
-        self.data = dict() ### equivalently, self.data = {}
-
+    #sychronizes self.data with setupDictionary according to the appropriate setup of fibers and feedthroughs
     def completeSetup(self, setup, setupDictionary):
-        ### sychronizes self.data with setupDictionary according to the appropriate setup of fibers and feedthroughs
         self.setup = setup
         for col in kc_dict[setup]:
             if col in setupDictionary:
@@ -115,54 +112,48 @@ class dataPoint(object):
             else:
                 self.data[col] = None
 
+    #reads in columns from the command line
     def readFromTerminal(self):
-        ### a method to read in columns from the command line
-
-        ### iterate over the known columns and read stuff in from the terminal
-        ### we "zip" together the column names and their types to make the iteration "simpler"
+        #iterate over the known columns and read stuff in from the terminal
+        #we "zip" together the column names and their types to make the iteration "simpler"
         for cast2type, col in zip(known_columns_types, known_columns):
             if self.data[col] == None:
-                self.data[col] = cast2type( raw_input("what is the value for  "+col+" : ") )
+                self.data[col] = cast2type(raw_input("what is the value for  "+col+" : "))
             else:
                 pass
 
+    #prints to terminal
     def printToTerminal(self):
-        ### a method to print to terminal
         print str(self.name) + " " + str(self.setup)
         for col in kc_dict[self.setup]:
             print "    value stored for "+col+" : "+str(self.data[col])
 
-#----------------------------------
+#---------------------------------------------------------------------
 
 def readSetup(setup):
-    
     setupDictionary[setup] = setup
     for col in kc_setup_dict[setup]:
-        setupDictionary[col] = str( raw_input("what is the value for  "+col+" : ") )
-    
+        setupDictionary[col] = str(raw_input("what is the value for  "+col+" : "))  
     return setupDictionary
-
-#a, b = makeMommaProud(1,1)
+    #a, b = makeMommaProud(1,1)
 
 def save(dataPoints):
-
     if not dataPoints:
         "There is nothing to save"
 
     else:
         filename = raw_input("what filename do you want to use to save the data?")
         file_obj = open(filename, "w")
-        #        print >> file_obj, "name " + "setup " +" ".join(known_columns)
+        #print >> file_obj, "name " + "setup " +" ".join(known_columns)
         for thisDataPoint in dataPoints:
-            #            print str(thisDataPoint.name)
+            #print str(thisDataPoint.name)
             #print str(thisDataPoint.setup)
             #print " ".join(str(thisDataPoint.data[col]) for col in kc_dict[thisDataPoint.setup])
             print >> file_obj, str(thisDataPoint.name) + " " + str(thisDataPoint.setup) + \
             " " + " ".join(str(thisDataPoint.data[col]) for col in kc_dict[thisDataPoint.setup])
         file_obj.close()
 
-
-def asignateNumber(fiber):
+def assignNumber(fiber):
     return int(list(fiber)[5])
 
 def getTime (dataPoint):
@@ -180,8 +171,7 @@ def definePower (inPower):
             #print str(inPower)
     return power
 
-
-# given a position and a dataPoint, gets the information about the fiber in this position
+#given a position and a dataPoint, gets the information about the fiber in this position
 def getFiberInfo(position, dataPoint):
     info = {}
     if position == 1:
@@ -199,29 +189,28 @@ def getFiberInfo(position, dataPoint):
 
     return info
 
-
-# returns a dictionary with the information (time, position, etc.) of a particular fiber, given a fiber and a data point.
-#returns and empty dictionary otherwise
+#returns a dictionary with the information (time, position, etc.) of a particular fiber, given the fiber and a data point.
+#returns and empty dictionary otherwise.
 def getFiberInDataPoint(dataPoint, fiber):
     fiberInDataPointInfo = {}
     if fiber not in known_fibers:
-        print "Sorry I do not know this fiber"
+        print "Sorry I do not recognize this fiber"
 
     else:
         for fib in known_fibers_setup:
             if fib in dataPoint.data.keys() and dataPoint.data[fib] == fiber:
-                fiberInDataPointInfo = getFiberInfo(asignateNumber(fib), dataPoint)
+                fiberInDataPointInfo = getFiberInfo(assignNumber(fib), dataPoint)
             else:
                 pass
 
     return fiberInDataPointInfo
 
-# goes thru a list of dataPoints extracting the information of a particular fiber
+#goes through a list of dataPoints extracting the information pertaining to a particular fiber
 def getFiberHistory (fiber, dataPoints):
-    fiberInfo = []
+    fiberInfo = [] #list of dictionaries
     actualFiber = {}
     if fiber not in known_fibers:
-        print "Sorry I do not know this fiber"
+        print "Sorry I do not recognize this fiber"
     else:
         for point in dataPoints:
             if point.setup == "STOP":
@@ -235,7 +224,7 @@ def getFiberHistory (fiber, dataPoints):
     return fiberInfo
 
 
-# sets the type/color/etc. of lines of the plots
+#sets the type/color/etc. of lines of the plots
 def defineArgs(point):
     selectedColor = 'k'
     selectedStyle = ':'
@@ -248,24 +237,21 @@ def defineArgs(point):
     selectedStyle = line_connectors[point['firstConnector']]
     return {'color': selectedColor,'linestyle':selectedStyle, 'markerstyle':"none", 'label':None, 'linewidth':selectedWidth}
 
-
-# defines the plot to be drawn according to the power level
+#defines the plot to be drawn according to the power level
 def selectedPlot(power, listOfPlots):
     for i, plot in listOfPlots:
         if power >= powerList[i]['min'] and power < powerList[i]['max']:
             break
     return plot
 
-
-
-# sets the labels of the plots according to the list of power
+#sets the labels of the plots according to the list of power
 def setPlotLabels(listOfPlots):
     for i, plots in listOfPlots:
         plots.set_ylabel(powerList[i]['name'] + "Trans")
         plots.set_xlabel("Time")
 
 
-# defines and sets the list of plots according to the list of power
+#defines and sets the list of plots according to the list of power
 def createPlots():
     plotsList = []
     lenght = len(powerList)
@@ -276,14 +262,14 @@ def createPlots():
         i = i+1
     return plotsList
 
+#---------------------------------------------------------------------
 
-#-------------------------------------
 def plotMe(data):
     fig = plt.figure()
     plotList = createPlots()
-    #    ax_H = plt.subplot(2,1,1) #2rows 1 column 1at panel
-    #    ax_L = plt.subplot(2,1,2)
-    #    plotList = [ax_H, ax_L]
+    #ax_H = plt.subplot(2,1,1) #2 rows, 1 column, 1 at panel
+    #ax_L = plt.subplot(2,1,2)
+    #plotList = [ax_H, ax_L]
     selectedPlot = None
     passing = True
     totalTime = datetime.timedelta()
@@ -297,14 +283,11 @@ def plotMe(data):
     x = []
     y = []
     for point in data:
-        #        pass #EXTRACT WASTE
-        
         if 'NOT' in point:
             if not passing:
                 passing = True
                 kwargs = defineArgs(point)
                 selectPlot(previousPower, plotList).plot(x,y,**kwargs)
-    
         else:
             actualPower = definePower(point['power'])
             actualTime = point['time']
@@ -315,19 +298,16 @@ def plotMe(data):
             if actualPower != previousPower:
                 kwargs = defineArgs(point)
                 selectPlot(previousPower, plotList).plot(x,y,**kwargs)
-            
-                #           if passing:
-                #x.add(timeToValue(totalTime))
-                #else:
+                #if passing:
+                    #x.add(timeToValue(totalTime))
+                    #else:
             timeDifference = actualTime - previousTime
             totalTime =+ timeDifference
             x.add(timeToValue(totalTime))
             y.add(point['transmission'])
             previousTime = actualTime
             previousPower = actualPower
-
-        
-#            ax_H.plot(x,y,color=asignedColor,linestyle=asignedStyle, markerstyle="none", label=None, linewidth=asignedWidth)
+            #ax_H.plot(x,y,color=asignedColor,linestyle=asignedStyle, markerstyle="none", label=None, linewidth=asignedWidth)
 
     xmin = numpy.infty
     xmax = - numpy.infty
@@ -342,69 +322,62 @@ def plotMe(data):
     for x in plotList:
         x.set_xlim(xmin=xmin, xmax=xmax)
         x.set_ylim(ymin=0, ymax=100)
-#       x.grid(True)
+        #x.grid(True)
 
     setPlotLabels(plotList)
-
     '''
     ax_H.set_ylabel("High Power Trans")
     ax_L.set_ylabel("Low Power Trans")
     ax_H.set_xlabel("Time")
     ax_L.set_xlabel("Time")
-    ax_H.legend(loc="best") # best location
+    ax_H.legend(loc="best") #best location
     '''
-
     plt.savefig("FIGURE1.png") #jpg pdf ...
     plt.show()
-
     plt.close(fig)
 
+#---------------------------------------------------------------------
 
+#perform the main loop
 
-#---------------------------------------------------------------------------------------------------------
-
-
-### perform the main loop
-
-
-dataPoints = [] ## instantiate an empty list object
+dataPoints = [] #instantiate an empty list object
 setup = "No defined setup"
 setupDictionary = {}
-
 '''
 file_obj = open("fiber.txt", "r")
-    #        cols = file_obj.readline().strip().split()[1:]
+    #       cols = file_obj.readline().strip().split()[1:]
     #if cols != known_columns:
-    #    print "whoops! looks like the columns don't match what I expected"
+    #   print "whoops! looks like the columns don't match what I expected"
     #else:
 for line in file_obj:
     line = line.strip().split()
     newDataPoint = dataPoint(float(line[0]))
     newDataPoint.setup = str(line[1])
-    for ind, (cast2type, col) in enumerate( zip(kct_dict[newDataPoint.setup], kc_dict[newDataPoint.setup]) ):
-        newDataPoint.data[col] = cast2type( line[ind+2] )
-    dataPoints.append( newDataPoint )
+    for ind, (cast2type, col) in enumerate(zip(kct_dict[newDataPoint.setup], kc_dict[newDataPoint.setup])):
+        newDataPoint.data[col] = cast2type(line[ind+2])
+    dataPoints.append(newDataPoint)
 setup = newDataPoint.setup
 file_obj.close()
 
 plotMe(getFiberHistory("1A", dataPoints))
 '''
-#--------------------
 
+#---------------------------------------------------------------------
 
+#USER INTERFACE
 while True:
 
-    ### figure out what el matador wants to do
+    #figure out what user wants to do
     action = raw_input("what do you want to do? You can say one of ["+"/".join(known_actions)+"] ")
     while action not in known_actions:
-        action = raw_input( "I don't understand that. You can say one of ["+"/".join(known_actions)+"] ")
+        action = raw_input("I don't understand that. You can say one of ["+"/".join(known_actions)+"] ")
 
-    ### fork program based on what we want to do
+    #fork program based on what we want to do
     if action == "enterData":
         isSameSetup = raw_input("is it the same layout as before (%s)? y/n " %(setup))
 
         if isSameSetup == "n":
-            #            save(dataPoints)
+            #save(dataPoints)
             setup = raw_input("which setup are you using ["+"/".join(known_setups)+"] ")
 
             if setup in known_setups:
@@ -421,17 +394,16 @@ while True:
         else:
             print "I do not understand your answer, please say y/n"
         
-        newDataPoint = dataPoint( time.time())
+        newDataPoint = dataPoint(time.time())
         newDataPoint.completeSetup(setup, setupDictionary)
         newDataPoint.readFromTerminal()
-        dataPoints.append( newDataPoint )
+        dataPoints.append(newDataPoint)
 
     elif action == "printData":
         for dataPoint in dataPoints:
             dataPoint.printToTerminal()
 
     elif action == "makeFigure":
-        
         fiberToPlot = raw_input("which fiber do you want to plot? You can say one of ["+"/".join(known_fibers)+"] ")
         plotMe(getFiberHistory(fiberToPlot, dataPoints))
 
@@ -441,44 +413,43 @@ while True:
     elif action == "load":
         filename = raw_input("what filename do you want to load?")
         file_obj = open(filename, "r")
-        #        cols = file_obj.readline().strip().split()[1:]
+        #cols = file_obj.readline().strip().split()[1:]
         #if cols != known_columns:
-        #    print "whoops! looks like the columns don't match what I expected"
+           #print "whoops! looks like the columns don't match what I expected"
         #else:
         for line in file_obj:
             line = line.strip().split()
             newDataPoint = dataPoint(float(line[0]))
             newDataPoint.setup = str(line[1])
-            for ind, (cast2type, col) in enumerate( zip(kct_dict[newDataPoint.setup], kc_dict[newDataPoint.setup]) ):
-                    newDataPoint.data[col] = cast2type( line[ind+2] )
-            dataPoints.append( newDataPoint )
+            for ind, (cast2type, col) in enumerate(zip(kct_dict[newDataPoint.setup], kc_dict[newDataPoint.setup])):
+                    newDataPoint.data[col] = cast2type(line[ind+2])
+            dataPoints.append(newDataPoint)
         setup = newDataPoint.setup
         file_obj.close()
 
-
     elif action == "notifyStop":
-        newDataPoint = dataPoint( time.time())
+        newDataPoint = dataPoint(time.time())
         newDataPoint.setup = "STOP"
         newDataPoint.data = {'STOP': "STOP"}
-        dataPoints.append( newDataPoint )
+        dataPoints.append(newDataPoint)
 
     elif action == "exit":
         print "goodbye"
         break
         
-    time.sleep(1) ### magic number are evil, but this is pretty innocent. This will sleep for 1 second before continuing the loop 
+    time.sleep(1) #sleep for 1 second before continuing the loop 
 
 '''
         print "by \"makeFigure\" I assume you mean make a histogram of the values of \"b\" recorded"
         figname = raw_input("what do you want to call your figure?")
         
         fig = plt.figure()
-        ax = fig.gca() ### get current axis
+        ax = fig.gca() ###get current axis
 
-        ### extract the data I want toTHan plot
+        ###extract the data I want toTHan plot
         data = [d.data["b"] for d in dataPoints]
 
-        ax.hist( data, bins=10, label="b")        
+        ax.hist(data, bins=10, label="b")        
 
         ax.set_xlabel("b")
         ax.set_ylabel("count")
@@ -487,6 +458,6 @@ while True:
 
         ax.legend(loc='upper right')
 
-        fig.savefig( figname )
-        plt.close( fig )
+        fig.savefig(figname)
+        plt.close(fig)
 ''' 
